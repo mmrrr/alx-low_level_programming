85e0fbbc41321cbaabc12637a6e523c2ca418662
mmrrr <mmrr9524@gmail.com>
2023-08-15 02:19:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+
+	char o[] = "_putchar";
+	int ch;
+
+	for (ch = 0 ; ch < 8 ; ch++)
+		_putchar(o[ch]);
+	_putchar('\n');
+
+	return (0);
+}
